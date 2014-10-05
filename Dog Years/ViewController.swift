fff8147c1dd61a763eb5178f741e067db08236a2
crsmnd <crsmnd@gmail.com>
2014-10-05 04:13:22 -0300

    first commit

--- Dog Years/ViewController.swift
+++ Dog Years/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dogYearInput: UITextField!
+    @IBOutlet weak var outputYears: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func convertButton(sender: UIButton) {
+        
+        outputYears.text = "\(dogYearInput.text.toInt()! * 7) human years!"
+        outputYears.hidden = false
+        dogYearInput.resignFirstResponder()
+        dogYearInput.text = ""
+        
+        
+    }
+    
+    
+    
+    
 }
 
